36f8e06da75a73c0cf89905e6de9449eda4bbab5
Yinsong Chen <41629998+Salamenta@users.noreply.github.com>
2020-10-08 01:33:44 +1100

    Update DTOSubjectSubmission.java

--- src/main/java/DTO/DTOSubjectSubmission.java
+++ src/main/java/DTO/DTOSubjectSubmission.java
@@ -8,13 +8,13 @@ import java.util.List;
 
 public class DTOSubjectSubmission {
     public final String dataId;
-    public final String subjectId;
+    public final String subjectCode;
     public final String subjectName;
     public List<DTOExamSubmission> exams = new ArrayList<>();
 
     public DTOSubjectSubmission(Subject subject) {
         dataId = subject.getId();
-        subjectId = subject.getSubjectCode();
+        subjectCode = subject.getSubjectCode();
         subjectName = subject.getSubjectName();
         for (Exam exam: subject.getExams()){
             exams.add(new DTOExamSubmission(exam));
