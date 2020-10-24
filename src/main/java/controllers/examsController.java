97dcb248ea7ba41f0a03d3a0f7718de87c896dd2
Yinsong Chen <41629998+Salamenta@users.noreply.github.com>
2020-10-24 22:13:26 +1100

    Revert "fix"
    This reverts commit c677f0bde89ca9e4e7d7eb7c166f1b2553a5993e.

---
+++ src/main/java/controllers/examsController.java
@@ -0,0 +1,190 @@
+package controllers;
+
+import DTO.DTOSubjectSubmission;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import com.google.gson.Gson;
+import datamapper.InstructorMapper;
+import datamapper.StudentMapper;
+
+import domain.Subject;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import utils.TokenVerification;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@WebServlet("/api/exams-controller")
+public class examsController extends HttpServlet {
+    private static final long serialVersionUID = 2L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public examsController() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */ // /api/exam-controller?status=managing
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            String status = request.getParameter("status");
+            //System.out.println("QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ");
+            PrintWriter out = response.getWriter();
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.setHeader("Access-Control-Allow-Origin", "*");
+
+            String token = TokenVerification.getTokenFromHeader(request);
+            String userIdAndUserType = "";
+            //System.out.println("token: " + token);
+            if(token.equals("")){
+                JSONObject jsonObject = new JSONObject(String.format(
+                        "{\"code\":\"%s\"}",HttpServletResponse.SC_UNAUTHORIZED));
+                out.print(jsonObject);
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                out.flush();
+                return;
+            } else {
+                userIdAndUserType = TokenVerification.verifyToken(token);
+                //System.out.println("userId and User Type");
+                if(userIdAndUserType.equals("")){
+                    JSONObject jsonObject = new JSONObject(String.format(
+                            "{\"code\":\"%s\"}",HttpServletResponse.SC_UNAUTHORIZED));
+                    out.print(jsonObject);
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    out.flush();
+                    return;
+                }
+
+            }
+
+
+            String userId = userIdAndUserType.split(",", 2)[0];
+//        String userType = userIdAndUserType.split(",", 2)[1];
+
+            if(status.equals("managing")){
+                String jsonArray = manageExams(userId);
+                String newJsonArray = jsonArray.replace("\"id\":", "\"dataId\":");
+
+                out.print(newJsonArray);
+                //System.out.println(newJsonArray);
+                //System.out.println("this is printing JSONArray!!!!!!!!!!!!!!!!!!!");
+                response.setStatus(HttpServletResponse.SC_OK);
+                out.flush();
+                return;
+            } else if(status.equals("marking")){
+                String jsonArray = markExams(userId);
+                String newJsonArray = jsonArray.replace("\"id\":", "\"dataId\":")
+                        .replace("\"examAnswers\":", "\"submissions\":");
+                out.print(newJsonArray);
+                response.setStatus(HttpServletResponse.SC_OK);
+                out.flush();
+                return;
+            } else if(status.equals("taking")){
+                // get exams of a student who can take right now
+                String jsonArray =  takeExams(userId);
+                String newJsonArray = jsonArray.replace("\"id\":", "\"dataId\":");
+                out.print(newJsonArray);
+                response.setStatus(HttpServletResponse.SC_OK);
+                out.flush();
+                return;
+            } else {
+                JSONObject jsonObject = new JSONObject(String.format(
+                        "{\"code\":\"%s\"}",HttpServletResponse.SC_INTERNAL_SERVER_ERROR));
+                out.print(jsonObject);
+                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                out.flush();
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        //System.out.println("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+    }
+
+    private String manageExams(String userId){
+        List<Subject> subjects = InstructorMapper.getSingletonInstance().getManagingSubjects(userId);
+
+        String json = new Gson().toJson(subjects);
+        String newJsonArray = json.replace("\"id\":", "\"dataId\":");
+//        JSONArray jsonArray = new JSONArray(subjects);
+
+        return newJsonArray;
+    }
+
+    private String markExams(String userId){
+        List<Subject> subjects = InstructorMapper.getSingletonInstance().getMarkingSubjects(userId);
+        List<DTOSubjectSubmission> dtoSubjectSubmissions = new ArrayList<>();
+
+        if (subjects.size()>0) {
+            //System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+            dtoSubjectSubmissions = subjects.stream().map(DTOSubjectSubmission::new)
+                    .collect(Collectors.toList());
+        }else {
+            //System.out.println("bbbbbbbbbbbbbbbbbbbbbbbbb");
+        }
+
+        String json = new Gson().toJson(dtoSubjectSubmissions);
+//        JSONArray jsonArray = new JSONArray(jsonString);
+
+        return json;
+    }
+
+    private String takeExams(String userId){
+        List<Subject> subjects = StudentMapper.getSingletonInstance().getTakingSubjects(userId);
+
+        String json = new Gson().toJson(subjects);
+        String newJsonArray = json.replace("\"id\":", "\"dataId\":");
+
+//        JSONArray jsonArray = new JSONArray(jsonString);
+
+        return newJsonArray;
+    }
+
+    private String convertToJson(List<Subject> subjects){
+        ObjectMapper mapper = new ObjectMapper();
+        String subjectsJsonString = "[";
+        for(Subject subject: subjects){
+            try {
+                String jsonString = mapper.writeValueAsString(subject);
+//                System.out.println("jsonString!!! " + jsonString);
+                subjectsJsonString = subjectsJsonString + jsonString;
+                subjectsJsonString = subjectsJsonString + ",";
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        }
+        subjectsJsonString = subjectsJsonString.substring(0,subjectsJsonString.length()-1);
+        subjectsJsonString = subjectsJsonString + "]";
+        if(subjectsJsonString.length()<2){
+            subjectsJsonString = "[]";
+        }
+        return subjectsJsonString;
+    }
+    protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "authorization");
+    }
+}
+
