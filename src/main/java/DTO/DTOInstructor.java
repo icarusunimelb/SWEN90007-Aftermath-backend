0b0eb3e28f9649459bdba7e9520b9d34004f3c92
Yinsong Chen <41629998+Salamenta@users.noreply.github.com>
2020-10-28 18:07:19 +1100

    fix

--- src/main/java/DTO/DTOInstructor.java
+++ src/main/java/DTO/DTOInstructor.java
@@ -1,13 +1,15 @@
 package DTO;
 
 import domain.Instructor;
+import exceptions.RecordNotExistException;
+
 import java.util.stream.Collectors;
 
 public class DTOInstructor {
     public final String dataId;
     public final String lecturerName;
 
-    public DTOInstructor(Instructor instructor) {
+    public DTOInstructor(Instructor instructor) throws RecordNotExistException {
         dataId = instructor.getId();
         lecturerName = instructor.getName().toString();
     }
