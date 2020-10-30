5571d054a0ebf5e7e7cc702376aa05e3a789f803
Yinsong Chen <41629998+Salamenta@users.noreply.github.com>
2020-10-30 18:41:02 +1100

    Update CustomConfig.java

--- src/main/java/security/CustomConfig.java
+++ src/main/java/security/CustomConfig.java
@@ -1,6 +1,8 @@
 package security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.rsocket.RSocketSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,7 +13,10 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 public class CustomConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.addFilter(new CorsFilter())
+        http.csrf().disable()
+            .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS,"*").permitAll()
+            .and().addFilter(new CorsFilter())
             .addFilterAfter(
                 new AuthenFilter(), CorsFilter.class)
             .addFilterAfter(
