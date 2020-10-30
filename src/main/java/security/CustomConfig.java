5ca736cfb9c33e5a7b0e83337518477701832646
Yinsong Chen <41629998+Salamenta@users.noreply.github.com>
2020-10-30 19:12:46 +1100

    fix

--- src/main/java/security/CustomConfig.java
+++ src/main/java/security/CustomConfig.java
@@ -1,5 +1,6 @@
 package security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.rsocket.RSocketSecurity;
@@ -7,19 +8,27 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class CustomConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-            .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
-            .and().addFilter(new CorsFilter())
+        http.cors().and()
+            .addFilter(new CorsFilter())
             .addFilterAfter(
                 new AuthenFilter(), CorsFilter.class)
             .addFilterAfter(
                 new AuthorFilter(), AuthenFilter.class);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
     }
 }
